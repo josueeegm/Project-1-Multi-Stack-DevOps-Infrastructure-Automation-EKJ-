0cbb388c6f55ac2cc13576ea36b5a5080c140ba0
Eugene Asiedu <uginasiedu079@gmail.com>
2025-10-31 14:50:37 +0100

    added more variables

--- terraform/vpc_module/variables.tf
+++ terraform/vpc_module/variables.tf
@@ -1,10 +1,17 @@
 variable "aws_region" {
   type = string
   description = "Region to create the resources"
-  default = "us-east-1"
+  default = "us-west-1"
 }
 
 variable "cidr_block" {
   type = string
+  description = "value"
   default = "10.0.0.1/16"
+}
+
+variable "vpc_name" {
+  type = string
+  description = "Name of the VPC"
+  default = "main-vpc"
 }
