17067f6be2eed58cf47c0b924d37ab7d5dc9942b
Eugene Asiedu <uginasiedu079@gmail.com>
2025-10-31 14:50:06 +0100

    replaced hardcoded string with variables

--- terraform/vpc_module/main.tf
+++ terraform/vpc_module/main.tf
@@ -1,9 +1,14 @@
 #configure the AWS provider
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 #create VPC
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = var.vpc_name
+  }
 }
--- terraform/vpc_module/variables.tf
+++ terraform/vpc_module/variables.tf
@@ -5,5 +5,6 @@ variable "aws_region" {
 }
 
 variable "cidr_block" {
+  type = string
   default = "10.0.0.1/16"
 }
