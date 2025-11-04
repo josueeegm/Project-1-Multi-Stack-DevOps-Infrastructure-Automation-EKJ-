5b224e9845c8bf6d750a39e524a5624eeb754243
Eugene Asiedu <uginasiedu079@gmail.com>
2025-11-04 03:19:00 +0100

    added more outputs

--- terraform/modules/vpc_module/outputs.tf
+++ terraform/modules/vpc_module/outputs.tf
@@ -1,14 +1,14 @@
-output "vpc-id" {
+output "vpc_id" {
   description = "ID of the VPC"
   value = aws_vpc.main.id
 }
 
-output "public-subnet-id" {
+output "public_subnet_id" {
   description = "ID of the public subnet"
   value = aws_subnet.public.id
 }
 
-output "private-subnet-id" {
+output "private_subnet_id" {
   description = "ID of the private subnet"
   value = aws_subnet.private.id
 }
