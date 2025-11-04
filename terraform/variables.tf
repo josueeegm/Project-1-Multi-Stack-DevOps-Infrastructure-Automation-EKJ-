3f9f1d7f1dde4d44274f1af3bac39b13ecae9cca
Eugene Asiedu <uginasiedu079@gmail.com>
2025-11-04 03:18:04 +0100

    added more variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 // Beginner-friendly variables: you fill these in terraform.tfvars
 
-variable "region" {
+variable "aws_region" {
   description = "AWS region"
   type        = string
 }
@@ -31,4 +31,63 @@ variable "instance_type_pg" {
   description = "Instance type for postgres"
   type        = string
   default     = "t2.micro"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "CIDR for VPC"
+}
+
+variable "public_subnet_cidr_block" {
+  type        = string
+  description = "CIDR for public subnet"
+}
+
+variable "private_subnet_cidr_block" {
+  type        = string
+  description = "CIDR for private subnet"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "VPC name"
+}
+
+variable "private_subnet_name" {
+  type = string
+}
+
+variable "public_subnet_name" {
+  type = string
+}
+
+variable "public_insatnce_availability_zone" {
+  type = string
+}
+
+variable "private_insatnce_availability_zone" {
+  type = string
+}
+
+variable "internet_gateway_name" {
+  type = string
+}
+
+variable "route_table_name" {
+  type = string
+}
+
+variable "bastion_instance_name" {
+  description = "Name of bastion host instance"
+  type = string
+}
+
+variable "postgres_instance_name" {
+  description = "Name of postgres instance"
+  type = string
+}
+
+variable "redis_worker_instance_name" {
+  description = "Name of worker and redis instance"
+  type = string
 }
